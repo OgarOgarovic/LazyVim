c2a1d250062c5b1722fe7f5f6c08b8d99b88f23f
Ivan Vnučko <ivan@vnucko.com>
2025-04-30 21:18:17 +0200

    First django snippet

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>se", function()
+  require("luasnip.loaders").edit_snippet_files()
+end, { desc = "Edit LuaSnip Snippets" })
